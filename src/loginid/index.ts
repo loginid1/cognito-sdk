14c1eea16ed0e97d93c21a9eb4ca0b0240095163
braagaa <michael_braga@outlook.com>
2024-05-26 21:40:16 -0400

    added PasskeyAPI gateway as optional feature

--- src/loginid/index.ts
+++ src/loginid/index.ts
@@ -1,33 +1,42 @@
 import LoginID from "@loginid/websdk3";
+import CognitoPasskeyAPI from "../services/cognitoPasskeys";
 import {CognitoUserSession} from "amazon-cognito-identity-js";
 import {parseJwt} from "../utils/encodes";
-import {AuthCompleteRequestBody, AuthInit} from "./types";
 import Cognito, {
 	CustomAuthentication,
 	CustomAuthenticationOptions,
 } from "../cognito";
+import { 
+	PasskeyCollection,
+	PasskeyResult,
+} from "../services/models/cognitoPasskeys";
 
 /**
  * LoginIDCognitoWebSDK class provides methods for adding and signing in with a passkey using FIDO2 operations.
  */
 class LoginIDCognitoWebSDK {
 	private cognito: Cognito;
+	private passkeyApi: CognitoPasskeyAPI = new CognitoPasskeyAPI("");
 
 	/**
 	 * Constructor for the LoginIDCognitoWebSDK class.
 	 *
 	 * @param {string} userPoolId - The ID of the Cognito User Pool.
 	 * @param {string} clientId - The client ID associated with the User Pool.
+	 * @param {string} passkeyApiBaseUrl - The base URL for the Passkey API.
 	 */
-	constructor(userPoolId: string, clientId: string) {
+	constructor(userPoolId: string, clientId: string, passkeyApiBaseUrl = "") {
 		this.cognito = new Cognito(userPoolId, clientId);
+		if (passkeyApiBaseUrl) {
+			this.passkeyApi = new CognitoPasskeyAPI(passkeyApiBaseUrl);
+		}
 	}
 
 	/**
 	 * Adds a passkey for the specified username using FIDO2 create operation.
 	 *
 	 * @param {string} username - The username of the Cognito user.
-	 * @param {string} idToken - The ID token associated with the user.
+	 * @param {string} idToken - The Cognito ID token associated with the user.
 	 * @param {CustomAuthenticationOptions} options - Additional options for custom authentication.
 	 * @returns {Promise<CognitoUserSession>} - A promise resolving to the Cognito user session.
 	 */
@@ -94,20 +103,72 @@ class LoginIDCognitoWebSDK {
 	 * using the LoginID SDK. It leverages FIDO2 WebAuthn to provide a secure, 
 	 * usernameless authentication.
 	 *
-	 * @param {AuthInit} init - The initialization parameters for authentication.
 	 * @param {CustomAuthenticationOptions} options - Additional options for custom authentication.
-	 * @returns {Promise<AuthCompleteRequestBody>} - A promise resolving to the authentication complete request body.
+	 * @returns {Promise<CognitoUserSession>} - A promise resolving to the Cognito user session.
 	 */
 	public async signInWithConditionalUI(
-		init: AuthInit,
 		options?: CustomAuthenticationOptions
-	): Promise<AuthCompleteRequestBody> {
+	): Promise<CognitoUserSession> {
 		const lid = new LoginID({baseUrl: "", appId: ""});
 		const lidOptions = {
 			autoFill: true,
 			...options?.abortSignal && {abortSignal: options.abortSignal},
 		}
-		return await lid.getNavigatorCredential(init, lidOptions);
+		const init = await this.passkeyApi.passkeyAuthInit();
+		const publicKey: any = await lid.getNavigatorCredential(init, lidOptions);
+		const {jwtAccess} =await this.passkeyApi.passkeyAuthComplete(publicKey);
+		return await this.signInWithAccessToken(jwtAccess, options);
+	}
+
+	/**
+	 * Lists all passkeys for a given user.
+	 *
+	 * This method retrieves a collection of passkeys associated with the provided Cognito
+	 * idToken. It interacts with the passkey API to fetch the list.
+	 *
+	 * @param {string} idToken - The Cognito ID token of the user.
+	 * @returns {Promise<PasskeyCollection>} - A promise resolving to a collection of passkeys data.
+	 */
+	public async listPasskeys(
+		idToken: string
+	): Promise<PasskeyCollection> {
+		return await this.passkeyApi.listPasskeys(idToken);
+	}
+
+	/**
+	 * Renames a specified passkey.
+	 *
+	 * This method updates the name of a passkey identified by the passkeyId.
+	 * It requires the user's Cognito idToken and the new name for the passkey.
+	 *
+	 * @param {string} idToken - The Cognito ID token of the user.
+	 * @param {string} passkeyId - The unique identifier of the passkey.
+	 * @param {string} name - The new name for the passkey.
+	 * @returns {Promise<null>} - A promise resolving to null upon successful renaming.
+	 */
+	public async renamePasskey(
+		idToken: string,
+		passkeyId: string,
+		name: string
+	): Promise<null> {
+		return await this.passkeyApi.renamePasskey(idToken, passkeyId, name);
+	}
+
+	/**
+	 * Deletes a specified passkey.
+	 *
+	 * This method removes a passkey identified by the passkeyId from the user's account.
+	 * It requires the user's Cognito idToken and the unique identifier of the passkey.
+	 *
+	 * @param {string} idToken - The Cognito ID token of the user.
+	 * @param {string} passkeyId - The unique identifier of the passkey.
+	 * @returns {Promise<null>} - A promise resolving to null upon successful deletion.
+	 */
+	public async deletePasskey(
+		idToken: string,
+		passkeyId: string
+	): Promise<null> {
+		return await this.passkeyApi.deletePasskey(idToken, passkeyId);
 	}
 }
 
--- src/loginid/types.ts
+++
@@ -1,6 +0,0 @@
-import LoginID from "@loginid/websdk3";
-
-type GetNavigatorCredentialF = LoginID["getNavigatorCredential"];
-
-export type AuthInit = Parameters<GetNavigatorCredentialF>[0];
-export type AuthCompleteRequestBody = ReturnType<GetNavigatorCredentialF>;
